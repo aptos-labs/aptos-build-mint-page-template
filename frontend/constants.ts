c9e084abb3b2081539a5f2f57c583da068ab48f2
Blake Zimmerman <blake.zimmerman@icloud.com>
2024-09-04 17:22:02 -0400

    Update to latest module addresses

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,8 +1,8 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
 export const MODULE_ADDRESS =
   NETWORK === "mainnet"
-    ? "0x220db6a4c15bb47c5b26f8bed9ca841ded18927c3f49fdd82d10b4b0daa70bd4"
-    : "0xdc125359657ec85e751bb23a7d256507ad7ee2865375c266a89ab88e692140e4";
+    ? "0x96c192a4e3c529f0f6b3567f1281676012ce65ba4bb0a9b20b46dec4e371cccd"
+    : "0x220db6a4c15bb47c5b26f8bed9ca841ded18927c3f49fdd82d10b4b0daa70bd4";
 export const MODULE_NAME = "unmanaged_launchpad";
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
