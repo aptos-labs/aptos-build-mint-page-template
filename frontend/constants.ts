559c8c5b192edc553ce003e67bb9d3441f60c43f
Blake Zimmerman <blake.zimmerman@icloud.com>
2024-09-04 01:57:26 -0400

    Add mainnet module address

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,6 +1,8 @@
 export const NETWORK = import.meta.env.VITE_APP_NETWORK ?? "testnet";
 export const MODULE_ADDRESS =
-  NETWORK === "testnet" ? "0xdc125359657ec85e751bb23a7d256507ad7ee2865375c266a89ab88e692140e4" : "TODO";
+  NETWORK === "mainnet"
+    ? "0x220db6a4c15bb47c5b26f8bed9ca841ded18927c3f49fdd82d10b4b0daa70bd4"
+    : "0xdc125359657ec85e751bb23a7d256507ad7ee2865375c266a89ab88e692140e4";
 export const MODULE_NAME = "unmanaged_launchpad";
 export const COLLECTION_ADDRESS = import.meta.env.VITE_COLLECTION_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
